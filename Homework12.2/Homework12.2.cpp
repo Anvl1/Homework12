d8b0799c5812c8294f338f89d328b667cb93b601
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-06-18 14:55:43 +1000

    Add files via upload

---
+++ Homework12.2/Homework12.2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    std::ifstream inputFile("in.txt");
+
+    if (!inputFile)
+    {
+        std::cerr << "Не удалось открыть файл!" << std::endl;
+        return 1;
+    }
+
+    int size;
+    inputFile >> size;
+    if (size <= 0)
+    {
+        std::cerr << "Некорректный размер массива!" << std::endl;
+        return 1;
+    }
+
+    int* array = new int[size];
+    
+    for (int i = 0; i < size; ++i)
+    {
+        inputFile >> array[i];
+    }
+
+    inputFile.close();
+    
+    for (int i = size - 1; i >= 0; --i)
+    {
+        std::cout << array[i];
+        if (i != 0)
+        {
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    
+    delete[] array;
+    
+    return 0;
+}
