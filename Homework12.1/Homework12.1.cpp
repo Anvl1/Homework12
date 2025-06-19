f238d6c7062e133945380eb3ef9e68da076dc7e3
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-06-20 00:08:36 +1000

    Add files via upload

---
+++ Homework12.1/Homework12.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    std::ifstream inputFile("in.txt");
+
+    if (!inputFile)
+    {
+        std::cerr << "Не удалось открыть файл!" << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    while (inputFile >> word)
+    {
+        std::cout << word << std::endl;
+    }
+
+    inputFile.close();
+    return 0;
+}
